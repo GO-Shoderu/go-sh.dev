c5906a79dcc6fdeb65f9921a7790e2b455532cf1
GO-Shoderu <go.shoderu@gmail.com>
2024-02-12 12:40:08 +0200

    making the timer more discriptive and also testing the hook out

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const countdown = setInterval(function () {
   // Display the countdown
   document.getElementById(
     "countdown"
-  ).innerHTML = `${days}day(s) ${hours}hour(s): ${minutes}minute(s): ${seconds}second(s)`;
+  ).innerHTML = `${days}day(s) <br/> ${hours}hour(s): ${minutes}minute(s): ${seconds}second(s)`;
 
   // If the launch date has passed, display a message and clear the countdown
   if (distance < 0) {
