489994e733d1cd9e7a3f4a53701af21627c3da18
GO-Shoderu <go.shoderu@gmail.com>
2024-12-19 04:38:49 +0200

    Adding a countdown to the other relevant places

--- js/index.js
+++ js/index.js
@@ -19,10 +19,45 @@ const countdown = setInterval(function () {
     "countdown"
   ).innerHTML = `${days} d(s) ${hours} hr(s): ${minutes} min(s): ${seconds} sec(s)`;
 
+  document.getElementById(
+    "countdown_2"
+  ).innerHTML = `${days} d(s) ${hours} hr(s): ${minutes} min(s): ${seconds} sec(s)`;
+
   // If the launch date has passed, display a message and clear the countdown
   if (distance < 0) {
     clearInterval(countdown);
-    document.getElementById("countdown").innerHTML = "Website is live!";
+    document.getElementById("countdown").innerHTML = "Update should be live!";
   }
 }, 1000);
 
+// Function to show a popup message when the button is clicked
+function showPopup() {
+  // Create popup div
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  // Add content to popup
+  popup.innerHTML = `
+    <h5 class="headings text-center text-md-start">This feature is Coming soon!</h5>
+    <span class="close-btn text-center text-md-start">&times;</span>
+    <br>
+    <p>Upgrade Count down with go-sh.dev, I'm working hard to finish the development of this site.
+    <br><br>
+    An upgrade is on the way and it should be up and running in </p>
+    <div id="countdown_2" class="countdown"></div>
+  `;
+
+  // Append popup to document body
+  document.body.appendChild(popup);
+
+  // Close popup when close button is clicked
+  popup.querySelector(".close-btn").addEventListener("click", function () {
+    document.body.removeChild(popup);
+  });
+}
+
+// Get all elements with the class 'popup-trigger-btn' and attach the function to their click event
+const popupTriggerButtons = document.querySelectorAll(".popup-trigger-btn");
+popupTriggerButtons.forEach(function (button) {
+  button.addEventListener("click", showPopup);
+});
